1164d1c0cef074e3838b77056093d721ff7b5bff
Gasanova Tamara <solas93@mail.ru>
2024-10-02 01:49:04 +0300

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+import { access } from 'node:fs';
+import { rm } from 'node:fs';
+
 const remove = async () => {
-    // Write your code here 
+    const error =  new Error('FS operation failed');
+    try {
+        access('src/fs/files/fileToRemove.txt', (err) => {
+            if (err) {
+                throw error;
+            } else {
+                rm('src/fs/files/fileToRemove.txt', (err) => {
+                    if (err) throw error;
+                    console.log('Removed!');
+                })
+            }
+        })
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 await remove();
