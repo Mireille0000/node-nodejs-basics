b693cf834c8111fdaeaa598b7aa5705ebcb536ad
Gasanova Tamara <solas93@mail.ru>
2024-10-01 23:39:05 +0300

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { writeFile } from 'node:fs';
+import { stat } from 'node:fs';
+
 const create = async () => {
-    // Write your code here 
+    const pathFresh = 'src/fs/files/fresh.txt';
+    const error = new Error('FS operation failed');
+    try {
+        stat(pathFresh, (_, stats) => {
+            if (stats) {
+              throw error;
+            } else {
+                writeFile('src/fs/files/fresh.txt', 'I am fresh and young', (err) => {
+                    if (err) throw err;
+                    console.log('The file has been saved!');
+                });
+            }
+        })
+    } catch(err) {
+        console.log(err);
+    }
 };
 
 await create();
