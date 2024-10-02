578b24ed526fe1ab7dd4e2b6916bac4dbaf9a58b
Gasanova Tamara <solas93@mail.ru>
2024-10-03 01:06:07 +0300

    feat: implement cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const commandLineArgs = process.argv.slice(2, process.argv.length);
+    
+    return commandLineArgs.map((_, i, arr) => {
+        if( i % 2 === 0) {
+            console.log(`${arr[i]} ${arr[i+1]}`);
+        }
+        return null;
+    })
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+    const names = Object.keys(process.env);
+    const values = Object.values(process.env);
+    return names.map((name, i) => {
+        if (name.includes('RSS_')) {
+            console.log(`${name}=${values[i]}`);
+        }
+    });
 };
 
 parseEnv();
