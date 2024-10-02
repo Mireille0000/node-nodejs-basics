d555726491da662d1e0e1457deeb10dfdeb7e5b2
Gasanova Tamara <solas93@mail.ru>
2024-10-02 14:14:32 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import { access, readdir } from 'node:fs';
+
 const list = async () => {
-    // Write your code here 
+    const error =  new Error('FS operation failed');
+    try {
+        access('src/fs/files', (err) => {
+            if (err) {
+                throw error;
+            } else {
+                readdir('src/fs/files', {withFileTypes: true, recursive: true }, (err, files) => {
+                    if(err) throw err;
+                    const listOfFiles = files.map((file) => file.name);
+                    console.log(listOfFiles);
+                } )
+            }
+        })
+    } catch(err) {
+        console.log(err);
+    }
 };
 
 await list();
