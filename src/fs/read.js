c843bb19bc60bc92a681e84fb6dcdf221f1ab95c
Gasanova Tamara <solas93@mail.ru>
2024-10-02 14:28:50 +0300

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import { access, readFile } from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+    const error =  new Error('FS operation failed');
+    try {
+        access('src/fs/files/fileToRead.txt', (err) => {
+            if (err) {
+                throw error;
+            } else {
+                readFile('src/fs/files/fileToRead.txt', 'utf8', (err, content) => {
+                    if(err) throw error;
+                    console.log(content);
+                })
+            }
+        })
+    } catch(err) {
+        console.log(err);
+    }
 };
 
 await read();
