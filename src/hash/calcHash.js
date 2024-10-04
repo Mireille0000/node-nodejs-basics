2b29f9af31e07832628f9b121e419e9598face3f
Gasanova Tamara <solas93@mail.ru>
2024-10-05 00:07:12 +0300

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const getHash = (path) => new Promise((resolve, reject) => {
+        const hash = createHash('sha256');
+        const readable = createReadStream(path);
+        readable.on('error', reject);
+        readable.on('data', (dataChunk) => hash.update(dataChunk));
+        readable.on('end', () => resolve(hash.digest('hex')));
+       })
+    try {
+        const hash = await getHash('src/hash/files/fileToCalculateHashFor.txt');
+        console.log(hash);
+      } catch (err) {
+        console.log(err);
+      }
 };
 
 await calculateHash();
