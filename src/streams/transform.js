db48e3ab7a84cac8877377bc4b09a1826fa818d3
Gasanova Tamara <solas93@mail.ru>
2024-10-06 04:17:43 +0300

    feat: implement transform function(streams)

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,4 +1,19 @@
+import { Transform } from 'node:stream';
+
+
 const transform = async () => {
+    try {
+        const reverseDataStream = () => new Transform({transform(chunk, encoding = 'utf8', callback) {
+            const reverseChunk = chunk.toString().split('').reverse().join('');
+            callback(null, reverseChunk);
+        }});
+
+        const reverseFn = reverseDataStream();
+        process.stdin.pipe(reverseFn).pipe(process.stdout);
+    } catch (err) {
+        console.log(err);
+    }
+
     // Write your code here 
 };
 
