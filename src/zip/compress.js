4677ccb3d686c39e8465211016395cab6032ee58
Gasanova Tamara <solas93@mail.ru>
2024-10-06 17:08:05 +0300

    feat: implement compress and decompress functions(zip)

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,4 +1,15 @@
+import { createGzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+
 const compress = async () => {
+    const gzipObj = createGzip();
+    const readable = createReadStream('src/zip/files/fileToCompress.txt');
+    const writable = createWriteStream('src/zip/files/archive.gz');
+    try {
+        readable.pipe(gzipObj).pipe(writable);    
+    } catch (err) {
+        console.log(err);
+    }
     // Write your code here 
 };
 
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,4 +1,16 @@
+import { createUnzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+
 const decompress = async () => {
+    const unzipObj = createUnzip();
+    const readable = createReadStream('src/zip/files/archive.gz');
+    const writable = createWriteStream('src/zip/files/fileToCompress2.txt');
+    try {
+        console.log('hey?');
+        readable.pipe(unzipObj).pipe(writable); 
+    } catch (err) {
+        console.log(err);
+    }
     // Write your code here 
 };
 
