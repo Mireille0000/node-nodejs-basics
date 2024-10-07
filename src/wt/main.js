e43af0d86e93ff0cb23dd3ba5c16be46bc95072e
Gasanova Tamara <solas93@mail.ru>
2024-10-07 04:17:51 +0300

    feat: implement worker threads task(main and worker functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import { Worker } from 'worker_threads'
+import { cpus } from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const cores = cpus().length;
+    const workerThreads = [...Array(cores)];
+    try {
+        const results = Array.from(({length: workerThreads.length}), (_, i) => {
+            return new Promise((resolve, reject) => {
+                const worker = new Worker('./src/wt/worker.js', {workerData: {data: 10 + i}});
+                worker.on("message", (result) => {
+                    resolve({status: 'resolved', data: result});
+                })
+            
+                worker.on("error", () => {
+                    reject(workerThreads.push({status: 'error', data: null}));
+                });
+            })
+        });
+
+        Promise.all(results).then((res) => console.log(res));
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 await performCalculations();
